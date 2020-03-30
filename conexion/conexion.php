54e23c42cb0bfee885002ac9bcbd320335e9b676
pedro <pedroduran2710@gmail.com>
2020-03-30 14:08:54 -0400

    version 2.0

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,16 +1,16 @@
 <?php
 
-    $host = "";
-    $user = "";
-    $password = "";
-    $db = "";
+    $host = "b1x5gsrnvd20qtstdh7a-mysql.services.clever-cloud.com";
+    $user = "uuz7qv7ubxjmjey0";
+    $password = "AtpZT0LWpOtjefvezbWr";
+    $db = "b1x5gsrnvd20qtstdh7a";
 
     $con = mysqli_connect($host,$user,$password,$db);
 
     if(!$con){
-        //echo'No hay conexion con'.mysqli_error($con);
+        echo'No hay conexion con'.mysqli_error($con);
     }else{
-        //echo'CONECTO';
+        echo'CONECTO';
     }
 
 ?>
